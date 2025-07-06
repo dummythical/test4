21908852d775154ee71e1ed6372fe36cfe3870d4
dummythical <digitalselleretc1@gmail.com>
2025-07-07 05:48:47 +0800

    Fix certificate verification error by using Electron net

--- renderer.js
+++ renderer.js
@@ -1,15 +1,18 @@
-const https = require('https');
+const { net } = require('electron');
 
 function fetchMessage() {
-  https.get('https://192.168.1.21', res => {
+  const request = net.request('https://192.168.1.21');
+  request.on('response', res => {
     let data = '';
-    res.on('data', chunk => data += chunk);
+    res.on('data', chunk => (data += chunk));
     res.on('end', () => {
       document.getElementById('response').textContent = data;
     });
-  }).on('error', err => {
+  });
+  request.on('error', err => {
     document.getElementById('response').textContent = `Error: ${err.message}`;
   });
+  request.end();
 }
 
 window.onload = fetchMessage;
